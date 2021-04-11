b30a485a4b7c7f4f0b562e40c109959b02a7b298
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-04-11 10:55:52 +0900

    Update Day28RegExPatternsAndIntroToDatabases.kt

--- Day28RegExPatternsAndIntroToDatabases.kt
+++ Day28RegExPatternsAndIntroToDatabases.kt
@@ -2,9 +2,7 @@ fun main() {
     val firstNames = mutableListOf<String>()
     repeat(readLine().orEmpty().toInt()) {
         val (firstName, email) = readLine().orEmpty().split(' ')
-        if (email.endsWith("@gmail.com")) {
-            firstNames.add(firstName)
-        }
+        if (email.endsWith("@gmail.com")) firstNames.add(firstName)
     }
     firstNames.sorted().forEach(::println)
 }
